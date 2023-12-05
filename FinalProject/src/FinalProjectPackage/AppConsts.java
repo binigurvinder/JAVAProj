6a239e4d85c351e46b56b8d49e6afebf96b9b552
Gurvinder singh <gurvinderbinny12@gmail.com>
2023-12-05 17:04:30 -0600

    removed files

--- FinalProject/src/FinalProjectPackage/AppConsts.java
+++
@@ -1,9 +0,0 @@
-package FinalProjectPackage;
-
-public class AppConsts {
-
-	public static final String URL = "jdbc:mysql://localhost:3306/finalProject";
-	public static final String USERNAME = "root";
-	public static final String PASSWORD = "Bini240794@@@";
-
-}
