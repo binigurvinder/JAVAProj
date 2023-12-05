b1a7ef754f2a786db96c38bdbacd9039da84f5cb
Gurvinder singh <gurvinderbinny12@gmail.com>
2023-12-05 17:02:37 -0600

    added comments

--- BankProject/src/Backend/DatabaseConnection.java
+++ BankProject/src/Backend/DatabaseConnection.java
@@ -1,95 +1,62 @@
 package Backend;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.DriverManager;
-
+import java.sql.Statement;
 
 public class DatabaseConnection {
-	
-	
-	 
-		public static Connection Connection =null;
-		public static Statement Statement =null;
-	 
-	 
-		public static Connection getConnection()
-		{
-			try {
-				
-				if(Connection == null)
-				{
-					Connection = DriverManager.getConnection(AppConsts.URL,AppConsts.USERNAME,AppConsts.PASSWORD);
-					
-				}
-				
+
+	// Static variables for holding single instances of Connection and Statement.
+	public static Connection Connection = null;
+	public static Statement Statement = null;
+
+	// Method to establish or return an existing database connection.
+	public static Connection getConnection() {
+		try {
+			// Singleton pattern - ensures only one connection instance.
+			if (Connection == null) {
+				Connection = DriverManager.getConnection(AppConsts.URL, AppConsts.USERNAME, AppConsts.PASSWORD);
 			}
-			catch(Exception ex)
-			{
+		} catch (Exception ex) {
+			ex.printStackTrace(); // Exception handling with stack trace printing.
+		}
+		return Connection;
+	}
+
+	// Method to create a SQL Statement object, following the singleton pattern.
+	public static Statement createStatement() {
+		try {
+			if (Statement == null && Connection != null) {
+				Statement = Connection.createStatement();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null; // Returns null if statement creation fails.
+		}
+		return Statement;
+	}
+
+	// Method to close the database connection safely.
+	public static void closeConnection() {
+		if (Connection != null) {
+			try {
+				Connection.close();
+				Connection = null; // Nullify the connection after closing.
+			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
-			return Connection;
-			
 		}
-		
-		
-		
-		public static Statement createStatement()
-		{
+	}
+
+	// Method to close the SQL Statement safely.
+	public static void closeStatement() {
+		if (Statement != null) {
 			try {
-				
-				if(Statement == null && Connection !=null)
-				{
-					Statement=Connection.createStatement();
-				}
-				
-			}
-			catch(Exception ex)
-			{
+				Statement.close();
+				Statement = null; // Nullify the statement after closing.
+			} catch (Exception ex) {
 				ex.printStackTrace();
-				return null;
-			}
-			return Statement;
-			
-		}
-		
-		
-	
-		
-		
-		public static void closeConnection() 
-		{
-		
-			if(Connection!=null)
-			{
-				try 
-				{
-					Connection.close();
-					Connection=null;
-				}
-				catch(Exception ex)
-				{
-					ex.printStackTrace();
-				}
 			}
 		}
-		
-		
-
-		public static void closeStatement() {
-			if(Statement!=null)
-			{
-				try 
-				{
-					Statement.close();
-					Statement=null;
-				}
-				catch(Exception ex)
-				{
-					ex.printStackTrace();
-				}
-			}		
-		}
-
+	}
 }
